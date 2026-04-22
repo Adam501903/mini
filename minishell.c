9404f0b68e264e3f8ab0a4f4d9244300d991aef2
Adam <saddoudadam3@gmail.com>
2026-04-22 13:50:02 +0100

    2eme commit

--- minishell.c
+++ minishell.c
@@ -6,59 +6,109 @@
 #include <string.h> 
 #include <stdbool.h> 
 
-void readfile(char* search){
+void execute_external_command(char *args[]); // forward declaration
+
+void readfile(const char* search){
     FILE *file = fopen("help.txt", "r");
     if (file == NULL) {
         perror("Error opening file");
         return;
     }
+
     char line[256];
+
     if (strcmp(search, "*") == 0) {
         while (fgets(line, sizeof(line), file)) {
             printf("%s", line);
         }
     } else {
-        // Print specific section
-        char *symb = "~";
+        const char *symb = "~";
         int reading = 0;
+
         while (fgets(line, sizeof(line), file)) {
             if (strstr(line, search) != NULL) {
                 reading = 1;
             }
+
             if (strstr(line, symb) != NULL) {
                 reading = 0;
             }
+
             if (reading) {
                 printf("%s", line);
             }
         }
     }
+
     fclose(file);
 }
 
 void parse_command(char* command){
-    int i=0;
+    int i = 0;
     char *token = strtok(command, " \n");
-    char *args[20];
-    args[1]="*";
+    char *args[20] = {NULL};
+
     while (token != NULL){
-        args[i]=token;
-        i++;
+        args[i++] = token;
         token = strtok(NULL, " \n");
     }
-    if (strcmp(args[0],"exit")==0 ) exit(0);
-    else if (strcmp(args[0],"help")==0 ) {
-        if (args[1] != NULL) {
+    args[i] = NULL;
+
+    if (args[0] == NULL) return;
+
+    if (strcmp(args[0], "exit") == 0) exit(0);
+
+    else if (strcmp(args[0], "help") == 0) {
+        if (args[1] == NULL)
+            readfile("*");
+        else
             readfile(args[1]);
+    }
+
+    else if (strcmp(args[0], "cd") == 0) {
+        if (args[1] != NULL) {
+            if (chdir(args[1]) != 0)
+                perror("cd error");
+        } else {
+            printf("cd: missing argument\n");
         }
-}} 
+    }
+
+    else {
+        execute_external_command(args);
+    }
+}
+
+void execute_external_command(char *args[]) {
+    pid_t pid = fork();
+
+    if (pid == 0) {
+        if (execvp(args[0], args) == -1) {
+            perror("exec error");
+            exit(EXIT_FAILURE);
+        }
+    }
+    else if (pid > 0) {
+        wait(NULL);
+    }
+    else {
+        perror("fork error");
+    }
+}
 
 int main(){
-    char command[256];;
-    while (1) {
-        printf("minishell> ");
-        fgets(command,255,stdin);
-        parse_command(command); //à developper
+    char command[256];
 
+    while (1) {
+        printf("\nminishell> ");
+
+        if (fgets(command, 255, stdin) == NULL) {
+            printf("\n");
+            break;
+        }
+
+        parse_command(command);
     }
+
+    return 0;
 }
